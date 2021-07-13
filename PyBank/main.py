5e840e209adf637c5849bf54e8d459fc7678f55b
Samantha Perez <sjenn257@gmail.com>
2021-07-12 21:06:01 -0500

    added more 07.12

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,15 @@
 import os
 
 import csv
+from datetime import datetime
 
 csv_path=os.path.join("PyBank","Resources","budget_data.csv")
 
-with open(csv_path, encoding='UTF-8') as csvfile:
+with open(csv_path, newline='',encoding='UTF-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     csv_header = next(csvreader)
 
+    
     for row in csvreader:
-        print(row)
-
+        print(row[0],float(row[1]))
