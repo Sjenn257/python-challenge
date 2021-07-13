fe5aa6265abe8023061edb6c520830c38a2c49ab
Samantha Perez <sjenn257@gmail.com>
2021-07-12 21:24:48 -0500

    added totals

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,23 @@ with open(csv_path, newline='',encoding='UTF-8') as csvfile:
 
     csv_header = next(csvreader)
 
-    
+    net_profit=0
+    row_counter=0
+   
+   
+
     for row in csvreader:
-        print(row[0],float(row[1]))
+        # keep total running for each month
+        net_profit+=float(row[1])
+        row_counter+=1
+        print(row[0],float(row[1]),net_profit,row_counter)
+
+    average_change=round(float(net_profit/row_counter),2)
+
+    print("Financial Analysis")
+    print("-" * 28)
+    print(f"Total Months: {row_counter}")
+    print(f"Total: ${net_profit}")
+    print(f"Average Change: ${average_change}")
+    print("Greatest Increase in Profits: ")
+    print("Greatest Decrease in Profits: ")
