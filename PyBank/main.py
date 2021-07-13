37f32e2206e048447e437691c67b4a91818d7ab0
Samantha Perez <sjenn257@gmail.com>
2021-07-13 12:24:53 -0500

    trying_greatest_values

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,6 @@
 import os
 
 import csv
-from datetime import datetime
 
 csv_path=os.path.join("PyBank","Resources","budget_data.csv")
 
@@ -10,28 +9,49 @@ with open(csv_path, newline='',encoding='UTF-8') as csvfile:
 
     csv_header = next(csvreader)
 
-    net_profit=0
+    net_profit_total=0
     row_counter=0
-    row_diff=0
     change_diff=0
-    profit_change=0
+    prev_date=''
+    prev_profit=0
     cumul_change=0
+    great_inc_date=''
+    great_inc_profit=0
+    great_dec_date=''
+    great_dec_profit=0
    
     for row in csvreader:
-        net_profit+=round(float(row[1]),2)
+        net_profit_total+=round(float(row[1]),2)
         row_counter+=1
 
         if row_counter == 1:
             change_diff=float('0')
         else:
-            change_diff=float(row[1])-profit_change
+            change_diff=float(row[1])-prev_profit
+        
+        if prev_profit>float(row[1]):
+            great_inc_date=prev_date
+            great_inc_profit=prev_profit
+        else:
+            great_inc_date=row[0]
+            great_inc_profit=float(row[1])
+
+        #check difference between positive and negative floats
+        #may need to separate them
+        if prev_profit<float(row[1]):
+            great_dec_date=prev_date
+            great_dec_profit=prev_profit
+        else:
+            great_dec_date=row[0]
+            great_dec_profit=float(row[1])
         
         cumul_change+=change_diff
 
         #set up for next row calc
-        profit_change=float(row[1])
+        prev_profit_change=float(row[1])
+        prev_date=row[0]
 
-        print(row[0],float(row[1]),row_counter, change_diff, profit_change)
+        print(row[0],float(row[1]),row_counter, change_diff, prev_profit)
 
     average_change=round(cumul_change/float(row_counter),2)
     #works but need to figure out how to skip first data row for change or make it zero
@@ -39,7 +59,7 @@ with open(csv_path, newline='',encoding='UTF-8') as csvfile:
     print("Financial Analysis")
     print("-" * 28)
     print(f"Total Months: {row_counter}")
-    print(f"Total: ${net_profit}")
+    print(f"Total: ${net_profit_total}")
     print(f"Average Change: ${average_change}")
-    print("Greatest Increase in Profits: ")
-    print("Greatest Decrease in Profits: ")
+    print(f"Greatest Increase in Profits: {great_inc_date} (${great_inc_profit})")
+    print(f"Greatest Decrease in Profits: {great_dec_date} (${great_dec_profit})")
