17e2f0f5a57ebb3ca5b737ac786269cf28039ba8
Samantha Perez <sjenn257@gmail.com>
2021-07-13 11:54:22 -0500

    average_change_works

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,16 +12,29 @@ with open(csv_path, newline='',encoding='UTF-8') as csvfile:
 
     net_profit=0
     row_counter=0
+    row_diff=0
+    change_diff=0
+    profit_change=0
+    cumul_change=0
    
-   
-
     for row in csvreader:
-        # keep total running for each month
-        net_profit+=float(row[1])
+        net_profit+=round(float(row[1]),2)
         row_counter+=1
-        print(row[0],float(row[1]),net_profit,row_counter)
 
-    average_change=round(float(net_profit/row_counter),2)
+        if row_counter == 1:
+            change_diff=float('0')
+        else:
+            change_diff=float(row[1])-profit_change
+        
+        cumul_change+=change_diff
+
+        #set up for next row calc
+        profit_change=float(row[1])
+
+        print(row[0],float(row[1]),row_counter, change_diff, profit_change)
+
+    average_change=round(cumul_change/float(row_counter),2)
+    #works but need to figure out how to skip first data row for change or make it zero
 
     print("Financial Analysis")
     print("-" * 28)
